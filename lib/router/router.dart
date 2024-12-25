ccaf6ba2ad147a4237573caa18644f79be8dd88d
Makarasok24 <makarasok.it@gmail.com>
2024-12-26 03:57:46 +0700

    delete sreen

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,5 @@
 import 'package:flashcards/models/decks.dart';
 import 'package:flashcards/screen/home_page.dart';
-import 'package:flashcards/screen/log_in_screen.dart';
-import 'package:flashcards/screen/sign_up_screen.dart';
 import 'package:flashcards/screen/testing/test_page.dart';
 import 'package:flashcards/screen/welcome_screen.dart';
 import 'package:flashcards/widget/navigation_menu.dart';
@@ -9,8 +7,6 @@ import 'package:flashcards/widget/navigation_menu.dart';
 import 'package:flutter/material.dart';
 
 class Routes {
-  static const String logIn = '/login';
-  static const String signUp = '/signup';
   static const String welcome = '/welcome';
   static const String home = '/home';
   static const String navigationMenu = '/navBar';
@@ -19,10 +15,6 @@ class Routes {
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case logIn:
-        return MaterialPageRoute(builder: (context) => const LogInScreen());
-      case signUp:
-        return MaterialPageRoute(builder: (context) => const SignUp());
       case welcome:
         return MaterialPageRoute(builder: (context) => const Welcomescreen());
       case home:
